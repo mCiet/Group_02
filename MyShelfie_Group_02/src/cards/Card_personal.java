7fefa686aa5ad4bd049ebd543fc37a19db33bc4b
mCiet <m.ciet@studenti.unibg.it>
2023-05-09 17:12:57 +0200

    Creazione enum personal_goals

--- MyShelfie_Group_02/src/cards/Card_personal.java
+++ MyShelfie_Group_02/src/cards/Card_personal.java
@@ -2,4 +2,5 @@ package cards;
 
 public class Card_personal {
 
+	
 }
---
+++ MyShelfie_Group_02/src/cards/personal_goals.java
@@ -0,0 +1,17 @@
+package cards;
+
+public enum personal_goals {
+
+	GOALS1,
+	GOALS2,
+	GOALS3,
+	GOALS4,
+	GOALS5,
+	GOALS6,
+	GOALS7,
+	GOALS8,
+	GOALS9,
+	GOALS10,
+	GOALS11,
+	GOALS12;
+}

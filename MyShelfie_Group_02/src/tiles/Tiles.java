e3f7b1993a8ee64a542b4b00ceff8af379dabb99
matteociola <m.ciola@studenti.unibg.it>
2023-05-10 11:18:19 +0200

    Tiles
    Ho scritto nella classe un commento al riguardo, ditemi pure se questa operazione di ordine sia inutile o meno, potete anche eliminarla in caso, solo fatemi sapere

--- MyShelfie_Group_02/src/tiles/Tiles.java
+++ MyShelfie_Group_02/src/tiles/Tiles.java
@@ -1,6 +1,61 @@
 package tiles;
-
+/* ho creato pacchetto e classe tiles perchè nei folder di gioco li idnetifica come pedine, semplicemente, sposterei qui
+ * ciò che è nella classe Card_objects, fatemi sapere in caso si possa fare diversamente.
+ */
 public class Tiles {
+	
+	private int colour;
+	
+	public Tiles(int colour) {
+		this.colour = colour;
+	}
+	
+	
+	public boolean isPink () {
+		if(this.colour== 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isBlue () {
+		if(this.colour== 2) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public boolean isGreen () {
+		if(this.colour== 3) {
+			return true;
+		}
+		return false;
+		
+	}
+	public boolean isWhite () {
+		if(this.colour== 4) {
+			return true;
+		}
+		return false;
+		
+	}
+	public boolean isYellow () {
+		if(this.colour== 5) {
+			return true;
+		}
+		return false;
+		
+	}
+	public boolean isLightBlue () {
+		if(this.colour== 6) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+	
 	
 	
 	

b24c7846f63d9520ef8f71604b5e07637f273527
matteociola <m.ciola@studenti.unibg.it>
2023-05-10 12:11:05 +0200

    Tokens

--- MyShelfie_Group_02/src/tokens/Tokens.java
+++ MyShelfie_Group_02/src/tokens/Tokens.java
@@ -1,5 +1,44 @@
 package tokens;
 
 public class Tokens {
-
+	
+	private int score;
+	
+	
+	public Tokens(int score) {
+		this.score = score;
+		
+	}
+	
+	
+	public boolean isScoring_2(){
+		if(this.score==2) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public boolean isScoring_4() {
+		if(this.score==4) {
+			return true;
+		}
+		return false;
+	}
+	 
+	
+	public boolean isScoring_6() {
+		if(this.score==6) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isScoring_8() {
+		if(this.score==8) {
+			return true;
+		}
+		return false;
+	}
+ 
 }

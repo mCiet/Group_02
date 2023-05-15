e33624da9bfac46f2a46dcf8d457cfbdaf5e59d3
LorenzoGambirasio <126600550+LorenzoGambirasio@users.noreply.github.com>
2023-05-15 11:34:38 +0200

    Controllo nGiocatori main

--- MyShelfie_Group_02/src/main/Main.java
+++ MyShelfie_Group_02/src/main/Main.java
@@ -1,5 +1,26 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int nGiocatori=0;
+		int f =0;
+		do {
+			System.out.println("Inserire numero di giocatori da 2 a 4 : ");
+			nGiocatori=scn.nextInt();
+			System.out.println(nGiocatori);
+			
+		}while(nGiocatori < 2 || nGiocatori > 4  );
+		
+		scn.close();
+		
+		// passaggio del numero dei giocatori per creare soggiorno 
+		
+		// inizio gioco 
+
+	}
 
 }
